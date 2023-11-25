a4b8d76409f33cda2493ec5998d3802a38638b0c
chaeyoung <chae00901@naver.com>
2023-11-25 11:36:33 +0900

    Fix Failing FibonacciTestCases

--- fib.py
+++ fib.py
@@ -5,7 +5,9 @@ When given a position, the function returns the fibonacci at that position in th
 The zeroth number in the fibonacci sequence is 0. The first number is 1
 Negative numbers should return None
 """
-def fibonacci(position):
+
+#수정
+def fibonacci1(position):
   if position < 0:
     return None
   elif position == 0:
@@ -13,4 +15,4 @@ def fibonacci(position):
   elif position == 1:
     return 1
   else:
-    return fibonacci(position - 1) + fibonacci(position - 2)
+    return fibonacci1(position - 1) + fibonacci1(position - 2)
